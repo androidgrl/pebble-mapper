9212e483039ebcaeb6dff5972ae62a57172dadb6
androidgrl <kawaharajamie@gmail.com>
2015-09-06 11:59:50 -0600

    change to 50

--- app/controllers/direction_controller.rb
+++ app/controllers/direction_controller.rb
@@ -8,7 +8,7 @@ class DirectionController < ApplicationController
     parsed = JSON.parse(response.body)
     first_direction = parsed["routes"][0]["legs"][0]["steps"][0]["distance"]
     first_distance = first_direction["value"].to_i
-    if first_distance < 100
+    if first_distance < 50
       second_direction = parsed["routes"][0]["legs"][0]["steps"][1]["html_instructions"]
       if second_direction[/left/]
         render json: { direction: "left", distance: first_distance }.to_json
